6955bf98b7634cec49052a4d8a41d220734cef33
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2018-07-29 22:21:42 +0900

    条件付きレンダリングまで学んだ

---
+++ 06_ if_rendering/index.js
@@ -0,0 +1,21 @@
+var vm1 = new Vue({
+  el: '#app1',
+  data: {
+    ok: true,
+    type: 'A'
+  }
+})
+
+var vm2 = new Vue({
+  el: '#app2',
+  data: {
+    loginType: 'username'
+  }
+})
+
+var vm3 = new Vue({
+  el: '#app3',
+  data: {
+    display: false
+  }
+})
